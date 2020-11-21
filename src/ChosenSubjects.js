5f75697c5696b3ec6611470a4c44a30a636e50a6
hypernek <70410498+hypernek@users.noreply.github.com>
2020-11-21 20:37:09 +0100

    načtení všech předmětů z JSON do tabulky
    použil jsem DataGrid komponentu

--- src/ChosenSubjects.js
+++ src/ChosenSubjects.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {makeStyles} from "@material-ui/core/styles";
 import {useSubjects} from "./hooks";
+import {DataGrid} from "@material-ui/data-grid";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -9,17 +10,51 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
+const sortModel = [
+    {
+        field: 'code',
+        sort: 'asc',
+    },
+];
+
+const getSubjectRows = subjectsFromJSON => {
+    // priprav predmety z JSONu pro zobrazeni v tabulce
+    for (var i = 0; i < subjectsFromJSON.length; i++)
+    {
+        subjectsFromJSON[i].id = i;
+    }
+    return subjectsFromJSON;
+}
+
+const getSubjectColumns = subjects => {
+    // priprav sloupce dynamicky
+    var columns = []
+    for (var prop in subjects[0])
+    {
+        if (prop != 'id')
+            columns.push(prop);
+    }
+    // nejak to serad
+    columns.sort();
+    columns = columns.map(x => {return {field: x.toString()}});
+    return columns;
+}
+
+
+
 /** Assigned to Hynek */
 // TODO Vypadá to jako job pro https://material-ui.com/components/tables/
 // TODO Tady je example jak dostat data: {subjects[0]["lecturer"]}
 const ChosenSubjects = () => {
-    const subjects = useSubjects();
+    const subjects = getSubjectRows(useSubjects());
     const classes = useStyles();
     return (
-        <div className={classes.root}>
-            Zde bude vyvíjen seznam vybraných předmětů, kam se umístí až bude potřeba ještě rozmyslíme.
-            Rozhodně to nebude takto přes one-way button "Vybrané předměty".
-            První předmět vyučuje: {subjects[0]["lecturer"]}
+        <div style={{ height: 550, width: '100%' }}>
+            <DataGrid
+                sortModel={sortModel}
+                columns={getSubjectColumns(subjects)}
+                rows= {subjects}
+            />
         </div>
     );
 };
