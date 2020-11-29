4330bec2deeebf1222bd3ca2d53680c9cd42f67f
hypernek <70410498+hypernek@users.noreply.github.com>
2020-11-29 13:50:07 +0100

    inteligentnější sloupce (chosenSubjectsColumns.js)
    Názvy sloupců nyní hardcoded (už se to nenačítá dynamicky z JSONu, nejspíš to nebude potřeba mít dynamicky). Názvy jsou v češtině a lze nastavit jejich šířku.

--- src/ChosenSubjects.js
+++ src/ChosenSubjects.js
@@ -3,6 +3,18 @@ import PropTypes from 'prop-types';
 import {makeStyles} from "@material-ui/core/styles";
 import {useSubjects} from "./hooks";
 import {DataGrid} from "@material-ui/data-grid";
+import {subjectColumns} from "./ChosenSubjectsColumns"
+
+// const subjectColumns = [
+//     //{ field: 'id', headerName: 'ID', width: 70 },
+//     { field: 'code', headerName: 'Kód', width: 100 },
+//     //{ field: 'name', headerName: 'Název', width: 150 },
+//     { field: 'lecturer', headerName: 'Vyučující', width: 200 },
+//     { field: 'length', headerName: 'Dotace', type: 'number', width: 100 },
+//     { field: 'day', headerName: 'Den', type: 'number',  width: 100 },
+//     //{ field: 'time', headerName: 'Čas', width: 100 },
+//     { field: 'room', headerName: 'Místnost', width: 150 }
+// ];
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -19,6 +31,7 @@ const sortModel = [
 
 const getSubjectRows = subjectsFromJSON => {
     // priprav predmety z JSONu pro zobrazeni v tabulce
+    // pro zobrazeni v tabulce je nutne, aby kazdy zaznam mel ciselne ID, to pak urcuje default sort
     for (var i = 0; i < subjectsFromJSON.length; i++)
     {
         subjectsFromJSON[i].id = i;
@@ -27,7 +40,7 @@ const getSubjectRows = subjectsFromJSON => {
 }
 
 const getSubjectColumns = subjects => {
-    // priprav sloupce dynamicky
+    // priprav sloupce dynamicky z dat
     var columns = []
     for (var prop in subjects[0])
     {
@@ -36,11 +49,10 @@ const getSubjectColumns = subjects => {
     }
     // nejak to serad
     columns.sort();
+    // takhle to DataGrid vyzaduje - sloupce tabulky jsou samostatne objekty
     columns = columns.map(x => {return {field: x.toString()}});
     return columns;
 }
-
-
 
 /** Assigned to Hynek */
 // TODO Vypadá to jako job pro https://material-ui.com/components/tables/
@@ -52,7 +64,7 @@ const ChosenSubjects = () => {
         <div style={{ height: 550, width: '100%' }}>
             <DataGrid
                 sortModel={sortModel}
-                columns={getSubjectColumns(subjects)}
+                columns={subjectColumns}
                 rows= {subjects}
             />
         </div>
---
+++ src/ChosenSubjectsColumns.js
@@ -0,0 +1,13 @@
+
+// sloupce bude lepsi vybrat takto hardcoded
+// jak budou vypadat sloupce ve vybranych predmetech
+export const subjectColumns = [
+    //{ field: 'id', headerName: 'ID', width: 70 },
+    { field: 'code', headerName: 'Kód', width: 100 },
+    { field: 'name', headerName: 'Název předmětu', width: 200 },
+    { field: 'lecturer', headerName: 'Vyučující', width: 150 },
+    { field: 'length', headerName: 'Délka', type: 'number', width: 100 },
+    { field: 'day', headerName: 'Den', type: 'number',  width: 100 },
+    { field: 'time', headerName: 'Čas', width: 100 },
+    { field: 'room', headerName: 'Místnost', width: 150 }
+];
