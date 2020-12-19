8505ab1c810b6cfc0b330b3a6f9a7ec65589a7af
Ajkub Gehrtenab <73476034+agehrtenab@users.noreply.github.com>
2020-12-19 01:27:43 +0100

    #3: Add docstrings, add more subject code exceptions, add subject summaries.

--- data/import_data.py
+++ data/import_data.py
@@ -19,37 +19,53 @@ async def fetch_xml(session, url):
 
 
 def fixup_specialization_name(s):
+    """Fix the irregular formatting of specialization names found in Bila kniha."""
     s = re.sub(" +", " ", s)
     return s.replace("jaderné", "Jaderné")
 
+def lecture_and_exercise_hours(rozsah_string):
+    """Attempt to parse the rozsah field in Bila kniha as the respective weekly lecture and exercise hours."""
+    m=re.match("^(?:(\d+)P?\+)?(\d+)C?$", rozsah_string)
+    if m:
+        return int(m[1]) if m[1] else 0, int(m[2])
+    else:
+        return 0,0
 
 def subject_codes(document):
+    """Return the codes of all the subjects present in a timetable XML document."""
     return document.xpath("/definitions/courses/course/acronym/text()")
 
 
 def get_course(document, course_acronym):
+    """Retrieve a course from a timetable XML document. In case of ambiguity, guess the correct element based on
+    the maximum number of linked cards."""
     course_elements = document.xpath('/definitions/courses/course[acronym/text()="{}"]'.format(course_acronym))
     return max(course_elements, key=lambda course: len(unique_course_time_slots(course)))
 
 
 def course_lectures(course):
+    """Retrieve all lectures associated with a course."""
     return course.xpath('/definitions/lectures/lecture[course/@ref={}]'.format(course.attrib['id']))
 
 
 def lecture_part_ids(lecture):
+    """Retrieve the IDs of all parts that have at least one card of a given lecture in their timetable."""
     xpath = '/definitions/cards/card/part[parent::card[lecture[@ref="{}"]]]/@ref'.format(lecture.attrib['id'])
     return {int(x) for x in lecture.xpath(xpath)}
 
 
 def part_ids_for_course(course):
+    """Retrieve all groups of students (identified by part id) that have the course present in their timetable."""
     return set().union(*(lecture_part_ids(lecture) for lecture in course_lectures(course)))
 
 
 def is_card_valid(card):
+    """Check if a card is not garbage (i.e., actually refers to a time slot)."""
     return card.xpath("string(day/@ref)") != '' and card.xpath("string(time/@ref)") != ''
 
 
 def card_data(card):
+    """Extract the relevant data from a card element: Who teaches the lesson in which room and building at what time."""
     lecture_id = card.xpath("string(lecture/@ref)")
     lecturer_id = card.xpath("string(lecturer/@ref)")
     classroom_id = card.xpath("string(classroom/@ref)")
@@ -66,6 +82,8 @@ def card_data(card):
 
 
 def cards_for_course_and_part(course, part_id):
+    """Return all lessons that a group of students identified by part_id supposed to attend to satisfy the
+    requirements of a course. """
     lecture_ids = [int(lecture.attrib['id']) for lecture in course_lectures(course)]
     lecture_predicate = " or ".join(["lecture/@ref={}".format(lecture_id) for lecture_id in lecture_ids])
     return course.xpath("/definitions/cards/card[part/@ref={}][{}]".format(part_id, lecture_predicate))
@@ -73,6 +91,7 @@ def cards_for_course_and_part(course, part_id):
 
 @functools.lru_cache(1000)
 def unique_course_time_slots(course):
+    """Return all alternative combinations of lessons that a student of a course must attend, with all duplicates removed."""
     return {frozenset({card_data(card) for card in cards_for_course_and_part(course, part_id)
                        if is_card_valid(card)})
             for part_id in part_ids_for_course(course)}
@@ -80,6 +99,8 @@ def unique_course_time_slots(course):
 
 @functools.lru_cache(1000)
 def jsonable_time_slots(course):
+    """Return the alternative allowed combinations of lessons that a student of a course may attend to satisfy the
+    requirements of a course, in a form suitable for dumping in JSON format. """
     keys = ("od", "do", "typ", "vyucujici", "mistnost", "budova")
     time_slots_with_possible_garbage = ([dict(zip(keys, values)) for values in variant if values[0] != 0]
                                         for variant in unique_course_time_slots(course))
@@ -88,14 +109,31 @@ def jsonable_time_slots(course):
 
 @functools.lru_cache(1000)
 def translate_subject_code(timetable, subject_code):
+    """Attempt a translation from a Bila kniha subject code to the relevant timetable subject code."""
     exceptions = {"01VAM": "VAM/B", "01VAMB": "VAM/B",
                   "01LIP": "LIP/B", "01LIPB": "LIP/B",
                   "17EHJE": "ZEH",
                   "17DEZ": "DEZ", "17PTA": "DEZ",
                   "17JBEZ": "JBEZ/", "17JABE": "JBEZ/", "17ZBJE": "JBEZ/",
                   "17ELZ": "TCJ1", "17TCJ1": "TCJ1",
-                  "17PRE": "NRE"
-                  }
+                  "17PRE": "NRE",
+                  "02KOHOM": "KOHO",
+                  "16EZ": "EZ", "16EZB": "EZ", "16EZM": "EZ",
+                  "16HE": "HE", "16HEB": "HE", "16HEM": "HE",
+                  "16INZ": "INZ", "16INZB": "INZ",
+                  "16MEIZ": "MEIZ", "16MEZB": "MEIZ",
+                  "16MERV": "16PMM",
+                  "16PAFZ1": "ZOME", "16PAFZB": "ZOME", "16ZOME": "ZOME", "16ZOMEM": "ZOME",
+                  "16RAO": "RAO", "16RAOB": "RAO",
+                  "16RDKP": "RDKP", "16RDKB": "RDKP", "16RDKBS": "RDKP",
+                  "16RTNM": "RFNM", "16RTNMM": "RFNM", "16RFNM": "RFNM", "16RFNMN": "RFNM",
+                  "16RTDG": "RFRD", "16RTDGM": "RFRD", "16RFRD": "RFRD",
+                  "16SEM1": "SEM", "16SED1": "SEM",
+                  "16UAZ": "UAZ", "16UAZB": "UAZ",
+                  "16ZBAF1": "ZBAF1", "16OAF1": "ZBAF1",
+                  "16ZJT": "ZJT", "16ZJTB": "ZJT",
+                  "16ZPP": "ZPP", "16ZPPB": "ZPP",
+                  "16ZIVO": "ZIVO", "16ZIVB": "ZIVO"}
     if subject_code in exceptions:
         return exceptions[subject_code]
     elif subject_code.lstrip(string.digits) in subject_codes(timetable):
@@ -106,31 +144,48 @@ def translate_subject_code(timetable, subject_code):
 
 @functools.lru_cache(maxsize=1000)
 def get_timetable_data(timetable, subject_code):
+    """Attempt to extract timetable data for a course identified by its code in Bila kniha."""
     possible_match = translate_subject_code(timetable, subject_code)
     return [] if possible_match is None else jsonable_time_slots(get_course(timetable, possible_match))
 
 
 def guess_semester_code():
+    """Guess the current semester code, such as 'B202'."""
     now = datetime.datetime.now()
     return "B{}{}".format(str(now.year % 100), 2 if now.month < 8 else 1)
 
 
 def guess_timetable_uri():
+    """Guess the URI at which an XML timetable is supposed to be published."""
     now = datetime.datetime.now()
     year = now.year % 100 - (1 if now.month < 8 else 0)
     semester = "letni" if now.month < 8 else "zimni"
     return "https://rozvrh.fjfi.cvut.cz/timetable/{}-{}-{}.xml".format(year, year + 1, semester)
 
+def summarize_rozvrhy(rozvrhy, key_fn, filter_fn=lambda card: True):
+    """Summarize a specific aspect of the timetable data (extracted by the provided key_fn function) for easier
+    searching, after optionally filtering out the irrelevant lesson cards."""
+    transformed_cards = ((key_fn(card) for card in card_list if filter_fn(card)) for card_list in rozvrhy)
+    return list(set(itertools.chain.from_iterable(transformed_cards)))
 
 def extract_subject_data(subject, timetable):
+    """Extract relevant data for a subject found in Bila kniha, potentially with its timetable entries if any are found."""
     subject_code = subject.xpath("string(kod)")
+    rozvrhy = get_timetable_data(timetable, subject_code)
+    lecture_hours, exercise_hours = lecture_and_exercise_hours(subject.xpath("string(rozsah)"))
     return {
         "predmet_id": int(subject.xpath("string(predmet_id)")),
         "kod": subject_code,
         "nazev": subject.xpath("string(nazev)"),
         "kredity": int(subject.xpath("string(kredity)")),
         "rozsah": subject.xpath("string(rozsah)"),
-        "rozvrhy": get_timetable_data(timetable, subject_code),
+        "tydne_prednasky": lecture_hours,
+        "tydne_cviceni": exercise_hours,
+        "rozvrhy": rozvrhy,
+        "vsechny_dny": summarize_rozvrhy(rozvrhy, lambda card: int(card["od"]//100)),
+        "vsichni_vyucujici": summarize_rozvrhy(rozvrhy, lambda card: card["vyucujici"]),
+        "vsichni_prednasejici": summarize_rozvrhy(rozvrhy, lambda card: card["vyucujici"], lambda card: card["typ"]=="P"),
+        "vsichni_cvicici": summarize_rozvrhy(rozvrhy, lambda card: card["vyucujici"], lambda card: card["typ"]=="C"),
         "zpuszak": subject.xpath("string(zpuszak)"),
         "anotace": subject.xpath("string(texty/anotace)").strip(),
         "osnova": subject.xpath("string(texty/osnova)").strip(),
@@ -141,11 +196,13 @@ def extract_subject_data(subject, timetable):
 
 
 def extract_study_plan(study_plan, timetable):
+    """Extract data for all subjects taught in a given study plan in the current semester."""
     for course in study_plan.xpath("//predmet[sem_idy/sem_idx/sem_id/text()=\"{}\"]".format(guess_semester_code())):
         yield extract_subject_data(course, timetable)
 
 
 def extract_everything(study_plans, timetable):
+    """Given all the study plans and the current timetable, return deduplicated data for all subjects in these plans."""
     duplicated_subjects = itertools.chain.from_iterable(extract_study_plan(plan, timetable) for plan in study_plans)
     return list(more_itertools.unique_everseen(duplicated_subjects, key=lambda subject: subject['predmet_id']))
 
